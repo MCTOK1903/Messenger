df4dc5bf4b81d24ffb488b9ee7c89cad91e0bc1e
MCT <celaltok2@gmail.com>
2020-07-09 17:05:23 +0300

    added sizing extension for UIView(width,top...)

---
+++ Messenger/Resources/Extentions.swift
@@ -0,0 +1,40 @@
+//
+//  Extentions.swift
+//  Messenger
+//
+//  Created by MCT on 9.07.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+      
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+    
+    
+}
