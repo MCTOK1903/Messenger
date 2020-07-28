116568c24cefe662de4bdfffded0b2e92ea06afa
MCT <celaltok2@gmail.com>
2020-07-28 23:07:51 +0300

    tableview added to the conversation page

---
+++ Messenger/Controllers/ChatViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatViewController.swift
+//  Messenger
+//
+//  Created by MCT on 28.07.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+}
--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
     
+    //MARK: - Properties
+    
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private let tableview: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Conversations"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
     
     //MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tableview)
+        view.addSubview(noConversationsLabel)
+        
+        setupTableView()
+        fetchConversations()
         
     }
     
@@ -34,5 +62,33 @@ class ConversationsViewController: UIViewController {
             present(nav, animated: false)
         }
     }
+    
+    private func setupTableView(){
+        tableview.delegate = self
+        tableview.dataSource = self
+    }
+    
+    private func fetchConversations() {
+        
+    }
+}
+
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "heyyo!"
+        return cell
+    }
+        
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
