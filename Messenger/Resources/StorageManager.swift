ce8eb7c851249d937d4cfe72fc2d9f7038449a03
MCT <celaltok2@gmail.com>
2020-08-17 12:28:51 +0300

    created downloadUrl func in StorageManager

--- Messenger/Resources/StorageManager.swift
+++ Messenger/Resources/StorageManager.swift
@@ -57,5 +57,21 @@ final class StorageManager {
         }
     }
     
+    ///returns the url of the given path
+    public func downloadUrl(for path:String, completion: @escaping(Result<URL, Error>)-> Void) {
+        
+        let reference = storage.child(path)
+        
+        reference.downloadURL { (url, error) in
+            guard let url = url, error == nil else {
+                completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                return
+            }
+            
+            completion(.success(url))
+        }
+        
+    }
+    
     
 }
